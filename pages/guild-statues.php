31515725ef3831862b9d40ecd9fda420edd05ed2
shadowprogram <myles.hann@gmail.com>
2022-06-13 19:28:59 -0230

    new commit

--- pages/guild-statues.php
+++ pages/guild-statues.php
@@ -7,7 +7,7 @@
 
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="../style.css">
 	</head>
 
 	<body>
@@ -16,8 +16,6 @@
 				<div class="container-fluid justify-content-center">
 					<ul class="nav navbar-nav">
 						<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-						<li class="nav-item"><a class="nav-link" href="pages/guild-roster.php">Guild Roster</a></li>
-						
 						<li class="nav-item"><a class="nav-link" href="pages/guild-statues.php">Guild Statues</a></li>
 						<li class="nav-item"><a class="nav-link" href="#">Guild Reckonings</a></li>
 						<li class="nav-item"><a class="nav-link" href="#">About</a></li>
@@ -27,31 +25,34 @@
 
 			<div class ="row">
 
-				<h1 class="auto-center text-center">Solaris Reckoning page</h1>
+				<h1 class="auto-center text-center">Guild Statue Input</h1>
 
 			</div>
 
 			<div class="row">
 				
-				<div id="reckoning-buttons" class="btn-group-vertical auto-center">
+				<form class="auto-center text-center">
+
+					<?php 
+
+						echo textInputHTML("player-name", "Name", "player-name"); 
+						echo textInputHTML("player-statue", "Exp", "player-statue");
+
+					?>
+
+				</form>
 				
-					<a href="#" role="button" aria-disabled="true" class="btn btn-primary">Ribbion Reckoning (not completed)</a>
-
-					<a href="ffiv_reckoning.php" role="button" aria-disabled="true" class="btn btn-primary">FFIV Reckoning</a>
-
-					<a href="ffx_reckoning.php" role="button" aria-disabled="true" class="btn btn-primary">FFX Reckoning</a>
-
-					<a href="moore_reckoning.php" role="button" aria-disabled="true" class="btn btn-primary">Moore Reckoning</a>
-
-					<a href="ffvii_reckoning.php" role="button" aria-disabled="true" class="btn btn-primary">FFVII Reckoning</a>
-
-					<a href="ffiv_reckoning.php" role="button" aria-disabled="true" class="btn btn-primary">FFIV Reckoning</a>
-
-
-				</div>
-
 			</div>
 
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	</body>
 </html>
+
+<?php
+
+function textInputHTML($id, $label, $name) {
+	$template = "<div class='form-group row'><label for='{$id}' class='col-sm-2'>{$label}</label><div class='col-sm-6'><input class='form-control' type='text' id='{$id}' name='{$name}'></div></div>";
+	return $template;
+}
+
+?>
